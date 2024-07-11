d51f8dd33fcff3434a4a22ed2e5dd806ba694be6
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-11 14:00:41 +0800

    feat(model): define TreeInfo model

--- api/gina/models.py
+++ api/gina/models.py
@@ -1,5 +1,12 @@
+from operator import truediv
+import uuid
 from django.db import models
 
 # Create your models here.
-
-
+class TreeInfo(models.Model):
+    tree_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tree_type = models.CharField(max_length=100, null=False)
+    tree_description = models.TextField()
+    tree_name = models.CharField(max_length=100, null=False)
+    scientific_name = models.CharField(max_length=100, null=False, unique=True)
+    family_name = models.CharField(max_length=100, null=False)
