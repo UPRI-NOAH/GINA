0031c682833d5b332ae69d571e953ab09864ae78
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-11 14:08:57 +0800

    feat(view): implement view for TreeInfo

--- api/gina/views.py
+++ api/gina/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets, mixins
+from api.gina.models import TreeInfo
+from api.gina.serializer import TreeInfoSerializer
 
 # Create your views here.
+class TreeInfoViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = TreeInfo.objects.order_by("tree_name")
+    serializer_class = TreeInfoSerializer
