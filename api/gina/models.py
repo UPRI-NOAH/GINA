8085a2c6ad5ef51147bd837a2479ef196fb1e7bd
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-11 14:41:59 +0800

    feat: allow nullable fields to be blank

--- api/gina/models.py
+++ api/gina/models.py
@@ -5,7 +5,7 @@ from django.db import models
     
 class TreeType(models.Model):
     type_name = models.CharField(max_length=100, primary_key=True)
-    type_description = models.TextField(null=True)
+    type_description = models.TextField(null=True, blank=True)
 
 def get_default_type():
     return TreeType.objects.get_or_create(type_name="default")
@@ -13,8 +13,8 @@ def get_default_type():
 class TreeInfo(models.Model):
     tree_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     tree_type = models.ForeignKey("TreeType", default=get_default_type, on_delete=models.SET_DEFAULT)
-    tree_description = models.TextField(null=True)
+    tree_description = models.TextField(null=True, blank=True)
     tree_name = models.CharField(max_length=100, null=False)
-    tree_image = models.URLField(max_length=200, null=True)
+    tree_image = models.URLField(max_length=200, null=True, blank=True)
     scientific_name = models.CharField(max_length=100, null=False, unique=True)
     family_name = models.CharField(max_length=100, null=False)
