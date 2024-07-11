c5a8ef7528c9f8d5466a35b461e6b9115726b466
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-11 14:37:28 +0800

    feat: add serializer and views for TreeType

--- api/gina/serializer.py
+++ api/gina/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from api.gina.models import TreeInfo
+from api.gina.models import TreeInfo, TreeType
 
 class TreeInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = TreeInfo
+        fields = '__all__'
+        
+class TreeTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreeType
         fields = '__all__'
--- api/gina/views.py
+++ api/gina/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets, mixins
-from api.gina.models import TreeInfo
-from api.gina.serializer import TreeInfoSerializer
+from api.gina.models import TreeInfo, TreeType
+from api.gina.serializer import TreeInfoSerializer, TreeTypeSerializer
 
 # Create your views here.
 class TreeInfoViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = TreeInfo.objects.order_by("tree_name")
     serializer_class = TreeInfoSerializer
+    
+class TreeTypeViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = TreeType.objects.order_by("type_name")
+    serializer_class = TreeTypeSerializer
