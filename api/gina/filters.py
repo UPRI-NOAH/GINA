900bdb5e915cef53c228e6e0a5983b0edee7b173
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-07-16 17:09:23 +0800

    feat: add filter for TreeType

--- api/gina/filters.py
+++ api/gina/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from api.gina.models import TreeInfo
+from api.gina.models import TreeInfo, TreeType
 
 class TreeInfoFilter(django_filters.FilterSet):
     class Meta:
@@ -10,4 +10,11 @@ class TreeInfoFilter(django_filters.FilterSet):
             "tree_name": ["iexact", "icontains"],
             "scientific_name": ["iexact", "icontains"],
             "family_name": ["iexact", "icontains"]
+        }
+
+class TreeTypeFilter(django_filters.FilterSet):
+    class Meta:
+        model = TreeType
+        fields = {
+            "type_name": ["iexact, icontains"]
         }
