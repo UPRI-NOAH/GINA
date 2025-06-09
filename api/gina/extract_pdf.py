3fd14c35efe3529cbbe00a05b3ec7a41077654a0
shannaurelle <shannaurelleg@gmail.com>
2025-06-09 18:15:47 +0800

    remove unnecessary regex filter

--- api/gina/extract_pdf.py
+++ api/gina/extract_pdf.py
@@ -16,42 +16,41 @@ doc = pymupdf.open("native-trees.pdf")
 #print(doc.metadata)
 
 # get page 42 of the PDF file
-plant = doc[41]
 
-find = plant.get_text("blocks")
+for page_num in range(25,31,2):
+    plant = doc[page_num]
 
-# extract the text from the block
-words = str(find[4][4])
+    find = plant.get_text("blocks")
 
-# debug commands
-# print(find[4])
+    # extract the text from the block
+    words = str(find[4][4])
 
-# remove space and line separators
-# for given name , scientific name,
-# and family name in that order
-words = re.sub("\s[\s\n]+","\n",words)
+    # debug commands
+    print(find[4])
 
-# remove family name description
-words = re.sub("\([A-Za-z\s]+\)","",words)
+    # remove space and line separators
+    # for given name , scientific name,
+    # and family name in that order
+    words = re.sub("\s[\s\n]+","\n",words)
 
-words = words.split("\n")
-words = [w for w in words if w != ""]
-print(words)
+    words = words.split("\n")
+    words = [w for w in words if w != ""]
+    print(words)
 
-for i, word in enumerate(words):
-    #  only extract the scientific name
-    #  and family name. Exclude the
-    #  name of person who discovered
-    #  the plant
-    if i == 1:
-        w1,w2,w3 = word.split(" ")
-        temp = w1 + " " + w2
-    else:
-        temp = word
-    temp = re.sub("\s+$","",temp)
-    if i < len(words) - 1:
-        print(temp,end=",")
-    else:
-        print(temp)
+    # for i, word in enumerate(words):
+        #  only extract the scientific name
+        #  and family name. Exclude the
+        #  name of person who discovered
+        #  the plant
+        # if i == 1:
+            # w1,w2,w3 = word.split(" ")
+            # temp = w1 + " " + w2
+        # else:
+            # temp = word
+        # temp = re.sub("\s+$","",temp)
+        # if i < len(words) - 1:
+            # print(temp,end=",")
+        # else:
+            # print(temp)
 
 
