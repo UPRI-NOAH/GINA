3d1167d1f55fe6d95bc7e5150c7bbf26d9e40d9f
shannaurelle <shannaurelleg@gmail.com>
2025-06-17 00:19:12 +0800

    docs: add instructions for running the merge_treeinfo script

--- api/gina/merge_treeinfo.py
+++ api/gina/merge_treeinfo.py
@@ -9,12 +9,9 @@ import re
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(PROJECT_ROOT))
 
-# cat the csv file
-# match header with treeinfo.csv
-# find all scientific names
-# filter all unique ones
-# append the unique ones to a new csv file
-# for testing 
+# HOW TO USE 
+# Run the command below on your command shell:
+# python api/gina/merge_treeinfo.py > api/gina/treeinfo-new.csv
 
 csv_1_file_path = 'api/gina/treeinfo.csv'
 df1 = pd.read_csv(csv_1_file_path)
@@ -27,15 +24,19 @@ print("tree_name,image,scientific_name,family_name,tree_description")
 scientific_names = [str(row['scientific_name']).upper() for index, row in df2.iterrows()]
 
 def print_treeinfo(row):
+
     tree_name = "" if pd.isnull(row['tree_name']) else row['tree_name']
     image = "" if pd.isnull(row['image']) else row['image']
     scientific_name = "" if pd.isnull(row['scientific_name']) else row['scientific_name']
     family_name = "" if pd.isnull(row['family_name']) else row['family_name']
     description = "" if pd.isnull(row['tree_description']) else row['tree_description']
+
     if scientific_name.count(',') > 0:
         scientific_name = "\"" + scientific_name + "\""
+
     if description.count(',') > 0:
         description = "\"" + description + "\""
+
     print(f"{tree_name},{image},{scientific_name},{family_name},{description}")
 
 for index, row in df2.iterrows():
