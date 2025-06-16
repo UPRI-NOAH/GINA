96ced1617c18085536c1fc0f1eef12a8e0db7a8b
shannaurelle <shannaurelleg@gmail.com>
2025-06-16 23:25:17 +0800

    feat: create initial merge treeinfo script template

--- api/gina/merge_treeinfo.py
+++ api/gina/merge_treeinfo.py
@@ -15,7 +15,15 @@ sys.path.append(str(PROJECT_ROOT))
 # append the unique ones to a new csv file
 # for testing 
 
-csv_header = input()
+csv_1_file_path = 'api/gina/treeinfo.csv'
+df1 = pd.read_csv(csv_1_file_path)
+csv_2_file_path = 'api/gina/treeinfo-partial.csv'
+def2 = pd.read_csv(csv_2_file_path)
 
 #print the header for new csv
-print("tree_name,image,scientific_name,family_name")
+print("tree_name,image,scientific_name,family_name,tree_description")
+
+scientific_names = 
+
+for index, row in df.iterrows():
+    
